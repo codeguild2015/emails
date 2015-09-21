529a8889ca222f444ce08fa90eaf749def337dc3
moranconnorj <moranconnorj@gmail.com>
2015-09-21 16:05:41 -0700

    initial commit

---
+++ wk1-0.py
@@ -0,0 +1,39 @@
+"""a = 2
+b = 4
+a, b = b, a"""
+
+"""def get_combo(n):
+	a = 1
+	for obj in range(1, n + 1):
+		a *= obj
+	return a
+print(get_combo(7))"""
+
+"""txt = 'but soft what light in yonder window breaks'
+words = txt.split()
+t = list()
+for word in words:
+	t.append((len(word), word))
+
+t.sort(reverse=True)
+
+res = list()
+for length, word in t:
+	res.append(word)
+
+print(res)"""
+
+get_count
+def get_email():
+	filename = input("input filename " )
+	file = open(filename, 'r')
+	for line in file:
+		x = line.strip().split()
+		for word in x:
+			if 'From' in word:
+				print(x[1], 
+
+get_count()
+
+	##put code here
+	##print(line)
