81fe8bcea2b3dceca31f6aad343b15c6cca4a0a4
rvarley <rvarley2001@yahoo.com>
2015-09-22 14:53:14 -0700

    new files for counting commit challeng

---
+++ tuple_ex1.py
@@ -0,0 +1,23 @@
+#filename = input('input filename '  )
+#file = open(filename, 'r')
+
+
+def parsefile(filename):
+    '''Inputs file containing mbox file
+    Outputs - list with elements containing addresses, and times stamps.'''
+
+    file = open(filename, 'r')
+    for line in file:
+        lst1 = line.split()
+        for words in lst1:
+            if lst1[0] == "From":
+                print(lst1)
+				
+	## put code here
+	#"print(line)
+	
+def count_user_commits():
+    '''Inputs - 
+
+
+parsefile('mbox-short.txt')
