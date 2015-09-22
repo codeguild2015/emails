a5afcac7294082e13cf01966f70462e40fdde5db
nsrchemie <nsrchemie@gmail.com>
2015-09-22 16:21:45 -0700

    fixed cipher

--- cipher.py
+++ cipher.py
@@ -7,28 +7,27 @@ def open_file():
 def count_dict(thing):
     k = 'zxcvbnmpoiuytrewqasdfghjkl'
     dict = {}
-    #thing.lower()
-    for elem in thing:
-        if elem in k:
-            if elem in dict:
-                dict[elem] += 1
-            else:
-                dict.update({elem:1})
+    for line in thing:
+        lower = line.lower()
+        for elem in lower:
+            if elem in k:
+                if elem in dict:
+                    dict[elem] += 1
+                else:
+                    dict.update({elem:1})
     return dict
 	
 def listify(dict):
     final_list = []
-    for letter, count in dict:
-        final_list.append([letter, count])
+    for letter, count in dict.items():
+        final_list.append([count, letter])
     final_list.sort()
     return final_list
 	
 def main():
     file = open_file()
     final_dict = count_dict(file)
-    print(final_dict)
     print_list = listify(final_dict)
     for elem in print_list:
         print(elem)
-	
 main()
