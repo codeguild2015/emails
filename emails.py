5e70a49c37b868c7fc3bc5e92f8454464454bcbf
Pjmcnally <Patrick.J.McNally@gmail.com>
2015-09-21 23:20:30 -0700

    renamed emails to original, modularizie emails

--- emails.py
+++ emails.py
@@ -1,25 +1,41 @@
-def tuple_full():
-    count_dict = {}
-    final_lst = []
+def open_file():
+    """Opens file and saves text as a variable"""
     filename = input("input filename "  )
     file = open(filename, 'r')
+    return file
 
+def list_split(file):
+    """Parses the .txt file, extracts all lines starting with "From" and appends them to a list."""
+    lst = []
     for line in file:
-        lst = line.split(" ")
-        if lst[0] == "From":
-            
+        if line.split(" ")[0] == "From":
+            lst.append(line.split(" "))
+    return lst
 
-            if lst[1] in count_dict:
-                count_dict[lst[1]] += 1
-            else:
-    	        count_dict.update({lst[1]: 1})
-    
-    for name, count in count_dict.items():
-        final_lst.append([count,name])
-    final_lst.sort(reverse=True)
-    
-    print(str(final_lst[0][0]) + " - " + final_lst[0][1])
-     
+def build_dict(lst):
+    count_dict = {}
+    """Returns a dictionary of lists with each containing count of commits and author"""
+    for item in lst:
+        if item[1] in count_dict:
+            count_dict[item[1]] += 1
+        else:
+            count_dict.update({item[1]: 1})
+    return count_dict
+
+def final(dict):
+    """Takes dictionary and returns sorted list"""
+    final_list = []
+    for name, count in dict.items():
+        final_list.append([count,name])
+    final_list.sort(reverse=True)
+    return final_list
 
 
-tuple_full()
+def main():
+    file = open_file()
+    lst = list_split(file)
+    count = build_dict(lst)
+    final_list = final(count)
+    print(str(final_list[0][0]) + " - " + final_list[0][1])
+
+main()
