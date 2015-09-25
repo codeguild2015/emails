42f86933ccce98af576e2dc29f90e43d0ecee649
Pjmcnally <Patrick.J.McNally@gmail.com>
2015-09-25 12:00:06 -0700

    update test file to be compatible with nosetest

--- emails_nick_patrick_test.py
+++ emails_nick_patrick_test.py
@@ -1,7 +1,5 @@
 from emails_nick_patrick import *
 
-
-
 def list_split_test_empty():
     assert list_split([]) == []
 
@@ -11,16 +9,15 @@ def list_split_test_from():
 def list_split_test_not_from():
     assert list_split(["Return-Path: <postmaster@collab.sakaiproject.org>"]) == []
 
-def build_count_dict_empty():
+def build_count_dict_test_empty():
     assert build_count_dict([]) == {}
 
-def build_count_dict_name():
+def build_count_dict_test_name():
     assert build_count_dict([["From", "stephen.marquard@uct.ac.za", "Sat", "Jan", "5", "09:14:16", "2008"]]) == {"stephen.marquard@uct.ac.za" : 1}
  
-def prep_most_last():
+def prep_most_test_last():
     assert prep_out_most({"Patrick" : 10, "Nick" : 20}) == ("Nick", 20)
 
-def prep_most_first():
+def prep_most_test_first():
     assert prep_out_most({"Patrick" : 35, "Nick" : 10}) == ("Patrick", 35)
 
-print("Success!")
